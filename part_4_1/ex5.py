a0cc75ed695a446ec8f25034c2964cda60584e0c
emailtoroi <emailtoroi@macbookpro.speedport.ip>
2025-12-14 21:23:02 +0100

    ex5.py

---
+++ part_4_1/ex5.py
@@ -0,0 +1,17 @@
+# Copy here code of line function from previous exercise
+def line(num, string):
+    if not string:
+        print("*" * num)
+    else:
+        print(string[0] * num)
+
+def square(size, character):
+    i = 0
+    while i < size:
+        line(size, character)
+        i += 1
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    square(5, "x")
+
